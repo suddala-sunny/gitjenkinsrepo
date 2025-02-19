c6661a8eddb70627bdd3578167d8db8151f1bbe8
Suddala Sunny <sunny123_nist@yahoo.co.in>
2025-02-19 11:40:40 +0530

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add Item Here'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
